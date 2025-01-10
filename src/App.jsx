249d3124461b2c321098b5d258ded0491a08174e
kronousKJ <kronocity@gmail.com>
2025-01-11 02:43:35 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MultiCurrencyTable } from './components/MultiCurrencyTable';
+
+function App() {
+  return (
+    <div className="p-6">
+      <MultiCurrencyTable />
+    </div>
+  );
+}
+
+export default App;
